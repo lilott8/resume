3d90c3578c067863efe90ff35ad4adea2143b538
lilott8 <lilot8@gmail.com>
2022-08-04 22:39:31 -0700

    simplified filtering and more content.

--- resume.js
+++ resume.js
@@ -1,38 +1,49 @@
-const types = new Map().set("resume", new Set(["summary", "technical", "experience-brief", "education", "publications", "awards"])).set( "cv", new Set(["summary", "technical", "experience-verbose", "education", "publications", "awards", "membership"]));
-// const types = new Set(["resume", "cv"]);
+const types = new Set(["resume", "cv"]);
 
 window.onload = function() {
     const params = new URLSearchParams(window.location.search);
     var type = "resume";
-    const hide = new Set();
+    const hideById = new Set();
+    const hideByClass = new Set();
 
-    if (params.get("hide")) {
-        for(const h of params.get("hide").split(',')) {
-            hide.add(h);
-        }
-    } else {
-        console.log("nothing to hide.");
-    }
-
-    console.log(hide);
-
-    var articles = document.getElementsByTagName("section");
-    console.log(articles);
-
+    // get the type of our resume.
     const typeArg = params.get("type");
     if (new Set(types.keys()).has(typeArg)) {
         type = typeArg;
     }
 
-    // const finalSet = filterForHiding(types.get(type), hide);
-    // console.log(finalSet);
-    hideElements(articles, hide);
+    Array.from(types).filter(item => item !== type).forEach(item => hideByClass.add(item));
+    console.log(hideByClass);
+
+    // See if there are any 
+    // additional things to hide.
+    if (params.get("hide")) {
+        for(const h of params.get("hide").split(',')) {
+            hideById.add(h);
+        }
+    }
+
+    console.log(hideById);
+
+    // Hide the elements that we don't need
+    // for this run of our resume/cv.
+    hideElements(type, hideById, hideByClass);
 };
 
-function hideElements(availableSections, toHide) {
-    for (const h of toHide) {
-        const element = document.getElementById(h);
-        element.style.display = "none";
+function hideElements(whitelist, hideById, hideByClass) {
+    for (const h of hideByClass) {
+        console.log(`hiding ${h}`);
+        const htmlElements = document.getElementsByClassName(h);
+        // We want to display anything that 
+        // should be filtered and is whitelisted.
+        Array.from(htmlElements).filter(item => !item.classList.contains(whitelist)).forEach(item => item.style.display = "none");
+    }
+    for (const h of hideById) {
+        console.log(`hiding ${h}`);
+        const htmlElement = document.getElementById(h);
+        if (htmlElement) {
+            htmlElement.style.display = "none";
+        }
     }
 }
 
